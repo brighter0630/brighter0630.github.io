72c6376d0aef918c1f959132e3850ccd6caec773
brighter0630 <brighter0630>
2023-08-13 11:42:55 +0900

    a

---
+++ _posts/knit.R
@@ -0,0 +1,12 @@
+library(knitr)
+filename <- "2023-08-12-아마존과-경쟁하는-CVS-기업분석"
+
+base.dir <- paste0("C:\\brighter0630.github.io\\_posts\\")
+base.url <- "/"
+fig.path <- paste0("./figure/", filename)
+paste0(base.dir, fig.path)
+opts_knit$set(base.dir = base.dir, base.url = base.url)
+opts_chunk$set(fig.path = fig.path) 
+setwd('C:\\brighter0630.github.io\\_posts\\')
+knit(paste0(filename, '.Rmd'), output = paste0(filename, '.md'))
+
