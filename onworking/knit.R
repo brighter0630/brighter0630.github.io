5dd5eea04d54f0241d338bcc974316106b6a7672
brighter0630 <brighter0630>
2023-08-13 11:12:04 +0900

    a

--- onworking/knit.R
+++ onworking/knit.R
@@ -3,9 +3,9 @@ filename <- "2023-08-12-아마존과-경쟁하는-CVS-기업분석"
 
 base.dir <- paste0("C:\\brighter0630.github.io\\")
 base.url <- "/"
-fig.path <- paste0("onworking/figure/", filename)
+fig.path <- paste0("/_posts/figure/", filename)
 paste0(base.dir, fig.path)
 opts_knit$set(base.dir = base.dir, base.url = base.url)
 opts_chunk$set(fig.path = fig.path) 
-setwd('C:\\brighter0630.github.io\\onworking\\')
-knit(paste0(filename, '.Rmd'))
+setwd('C:\\brighter0630.github.io\\')
+knit(paste0('onworking\\', filename, '.Rmd'), output = paste0('onworking\\', filename, '.md'))
