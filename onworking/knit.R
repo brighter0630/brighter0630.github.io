b8e027eff6022dbeeb03940dd1874160dee5e0af
brighter0630 <brighter0630>
2023-08-13 11:29:59 +0900

    a

--- onworking/knit.R
+++ onworking/knit.R
@@ -2,7 +2,7 @@ library(knitr)
 filename <- "2023-08-12-아마존과-경쟁하는-CVS-기업분석"
 
 base.dir <- paste0("C:\\brighter0630.github.io\\")
-base.url <- ""
+base.url <- "/"
 fig.path <- paste0("_posts/figure/", filename)
 paste0(base.dir, fig.path)
 opts_knit$set(base.dir = base.dir, base.url = base.url)
