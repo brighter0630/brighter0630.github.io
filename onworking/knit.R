a70f7865ad5979e3108b1bac0f0f96f4b63f598f
unknown <brighter87@naver.com>
2023-08-13 15:07:23 +0900

    a

---
+++ onworking/knit.R
@@ -0,0 +1,14 @@
+library(knitr)
+filename <- "2023-08-12-아마존과-경쟁하는-CVS-기업분석"
+
+base.dir <- paste0("C:\\brighter0630.github.io\\")
+base.url <- "../"
+fig.path <- paste0("images/", filename)
+paste0(base.dir, fig.path)
+opts_knit$set(base.dir = base.dir, base.url = base.url)
+opts_chunk$set(fig.path = fig.path) 
+setwd('C:\\brighter0630.github.io\\')
+
+knit(input = paste0('onworking/', filename, '.Rmd'),
+     output = paste0('_posts/', filename, '.md'),
+     encoding = 'UTF-8')
